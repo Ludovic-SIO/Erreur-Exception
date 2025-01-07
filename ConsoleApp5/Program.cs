91f7944be132839d336853d277bc14a6151be0bf
lchamprobert <lchamprobert@N112-04>
2025-01-07 10:37:01 +0100

    Ajoutez des fichiers projet.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            bool correct = true;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Saisie un âge ");
+                    a = Convert.ToInt32(Console.ReadLine());
+                    while (a > 110||a<0)
+                    {
+                        Console.WriteLine("Erreur ");
+                        a = Convert.ToInt32(Console.ReadLine());
+                    } 
+                }
+                catch (FormatException)
+                { 
+                    Console.WriteLine("Erreur de Format");
+                }
+                finally
+                {
+                    Console.ReadKey();
+                }
+            }
+            while (correct == true);
+        }
+    }
+}
